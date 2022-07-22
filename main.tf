fdcafc4012bdfa2620ce86b0b3663a23211a82a3
panyergo <69608717+panyergo@users.noreply.github.com>
2022-07-22 06:53:57 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "aws_s3_bucket" "data" {
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
+  # Comment deleteme
   bucket        = "${local.resource_prefix.value}-data"
   region        = "us-west-2"
   acl           = "public-read"
